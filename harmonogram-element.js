010b45b02fda1192cea7fad30df6ceb3595672ef
Jan Marcaník <jan@czechmtbholidays.com>
2026-05-13 00:05:46 +0200

    Update harmonogram-element.js

--- harmonogram-element.js
+++ harmonogram-element.js
@@ -347,8 +347,8 @@
     { day: "sat", allDay: false, time: "19:15", duration: 25, type: "talkshow",
       title: { cs: "Talk Show Daniela Stacha",
                en: "Talk Show with Daniel Stach" },
-      desc:  { cs: "Vavřinec Hradilek v ROBE Show Truck pohovoří o tom, jak si užívá sportovní "důchod", povypráví o svých úspěších v reality show Asia Express a určitě také pohovoří na téma já a mé kolo.",
-               en: "Former Olympic medallist Vavřinec Hradilek in ROBE Show Truck will talk about his sports retirement, successful appearance in the Asia Express reality show and definitely also about his beloved bike." } },
+      desc:  { cs: "Vavřinec Hradilek v ROBE Show Truck pohovoří o tom, jak si užívá sportovní "důchod", povypráví o svých úspěších v reality show Asia Express a určitě také pohovoří o svém vztahu k cyklistice.",
+               en: "Former Olympic medalist Vavřinec Hradilek in ROBE Show Truck will talk about his sports retirement, successful appearance in the Asia Express reality show and about his beloved bike." } },
 
     { day: "sat", allDay: false, time: "19:40", duration: 20, type: "talkshow",
       title: { cs: "Saxo ROBE music show - Radim Nowak",
